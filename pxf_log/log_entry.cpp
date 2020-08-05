9d8d0d34aef1139f8734133222e132ed3c8309df
Paul Jones <1019888+SenojLuap@users.noreply.github.com>
2020-08-05 19:26:31 -0400

    minor tweak

--- pxf_log/log_entry.cpp
+++ pxf_log/log_entry.cpp
@@ -11,9 +11,9 @@ namespace PXFLOG {
         std::stringstream res;
 
         auto* local = localtime(&time);
-        char time_buffer[9];
-        sprintf(time_buffer, "%02u:%02u:%02u", local->tm_hour, local->tm_min, local->tm_sec);
-        res << "[" << time_buffer << "]";
+        char time_buffer[11];
+        sprintf(time_buffer, "[%02u:%02u:%02u]", local->tm_hour, local->tm_min, local->tm_sec);
+        res << time_buffer;
 
         res << "<" << severity_to_string(severity) << "> ";
 
